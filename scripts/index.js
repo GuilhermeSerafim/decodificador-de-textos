429b7e18e427501aa1842fa5575a9d3dfc3b067d
GuilhermeSerafim <guilerstudies@gmail.com>
2023-07-01 19:04:33 -0300

    Adicionando a função de descriptografar

--- scripts/index.js
+++ scripts/index.js
@@ -23,3 +23,29 @@ function criptografar() {
         alert("Você deve digitar algum texto");
     }
 }
+
+function descriptografar() {
+    let texto = document.getElementById("texto").value;
+    let tituloMensagem = document.getElementById("titulo-mensagem");
+    let paragrafo = document.getElementById("paragrafo");
+    let boneco = document.getElementById("boneco");
+
+    let textoCriptografado = texto
+        .replace(/enter/gi, "e")
+        .replace(/imes/gi, "i")
+        .replace(/ai/gi, "a")
+        .replace(/ober/gi, "o")
+        .replace(/ufat/gi, "u");
+
+    if (texto.length != 0) {
+        document.getElementById("texto").value = textoCriptografado;
+        tituloMensagem.textContent = "Texto descriptografado com sucesso";
+        paragrafo.textContent = "";
+        boneco.src = "./img/descriptografado.png";
+    } else {
+        boneco.src = "./img/High quality products 1 1.png";
+        tituloMensagem.textContent = "Nenhuma mensagem foi encontrada";
+        paragrafo.textContent = "Digite um texto que você deseja criptografar ou descriptografar";
+        alert("Você deve digitar algum texto");
+    }
+}
